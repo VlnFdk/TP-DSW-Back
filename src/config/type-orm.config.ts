91afb1bf74a625b49522999ec6923537a582aa4b
VlnFdk <vlnfeldkircher@gmail.com>
2025-09-14 14:29:18 -0300

    config bd en .env

--- src/config/type-orm.config.ts
+++ src/config/type-orm.config.ts
@@ -2,13 +2,16 @@ import { DataSource } from "typeorm"
 import { Inmueble } from "../models/Inmueble.js";
 
 
+require('dotenv').config({path: 'src/config/.env'}); 
+
+
 const dbconfig = new DataSource({
-    type: "mysql",
-    host: "localhost",
-    port: 3306,
-    username: "root",
-    password: "root",
-    database: "api-express",
+    type: 'mysql',
+    host: process.env['HOST_ENV'] as string,
+    port: Number(process.env['PORT_ENV']),
+    username: process.env['USER_ENV'] as string,
+    password: process.env['PASSWORD_ENV'] as string,
+    database: process.env['BDNAME_ENV'] as string,
     logging: true,
     entities: [Inmueble],
     synchronize: true // SEGUN DOCUMENTACION, NO SE RECOMIENDA PARA PRODUCCION, SOLAMENTE DESARROLLO. PERMITE CREAR LA BD SI NO EXISTE.
